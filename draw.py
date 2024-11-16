c2ade0dbffa68ab2ad99bcdab3b97a9db1bd7160
Craig Bourne <craigbourne15@gmail.com>
2024-11-16 17:11:00 +0000

    added exercises

---
+++ draw.py
@@ -0,0 +1,5 @@
+'''
+Exercise 15.2. Write a function called draw_rect that takes a Turtle object and a Rectangle and uses the Turtle to draw the Rectangle. See Chapter 4 for examples using Turtle objects.
+
+Write a function called draw_circle that takes a Turtle and a Circle and draws the Circle. Solution: https: // thinkpython. com/ code/ draw. py.
+'''
